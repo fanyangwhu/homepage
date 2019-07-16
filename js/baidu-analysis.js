8b53c1d94065131f989845625ddae1345aa8b40a
yangfan <yangfan@icarbonx.com>
2019-07-16 16:07:24 +0800

    add baidu-analysis

---
+++ js/baidu-analysis.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b7b572404cfec5fb0dca8d0b543938dc";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
